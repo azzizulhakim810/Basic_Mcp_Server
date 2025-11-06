9169e370a292dcc40f414f86a2be2ac6ce5ea4b7
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-11-06 10:05:59 +0600

    Added a ping tool which response to your text

---
+++ src/server.ts
@@ -0,0 +1,50 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import z from "zod";
+
+const server = new McpServer({
+  name: "basics",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+  },
+});
+
+server.registerTool(
+  "ping",
+  {
+    title: "Ping Tool",
+    description:
+      "Echoes the text back so the host can verify the tool is reachable.",
+    inputSchema: {
+      text: z.string().optional(),
+    },
+  },
+  // handler Function
+  async ({ text }) => {
+    const message = text
+      ? `Pong - you said: ${text}`
+      : `Pong - No text provided`;
+    return {
+      content: [
+        {
+          type: "text",
+          text: message,
+        },
+      ],
+    };
+  }
+);
+
+// Start the MCP Server
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("MCP Server running on stdio transport");
+}
+
+main().catch((error) => {
+  console.error("Server Error", error);
+  process.exit(1);
+});
